eb27ae02d80b71b0bbf7dc536d67b75953f14d21
Yao Zhang <Yao.Zhang@gartner.com>
2021-07-19 20:12:56 -0400

    update the app and index html

--- node/app.js
+++ node/app.js
@@ -1,11 +1,21 @@
 const http = require('http');
+const fs =require('fs')
 const port = 3000
 
 const server = http.createServer(function(req, res){
-
+  res.writeHead(200, {'Content-type':'text/html'})
+  fs.readFile('index.html', (error,data)=>{
+    if(error){
+      res.writeHead(404)
+      res.write('Error:File Not Found')
+    }else{
+      res.write(data);
+    }
+    res.end();
+  })
 })
 
-server.listen(prot, function(error){
+server.listen(port, function(error){
   if(error){
     console.log("Something went wrong", error);
   }else{
